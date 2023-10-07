6c5a58288020d09f6918523d574d27e2a36d7a3a
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-10-07 21:39:06 +0900

    #1 [chore] dev branch 생성

---
+++ app/home/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex h-screen justify-center items-center">
+      <div className="flex h-3/6 w-screen border-b-2 border-gray-500"></div>
+    </div>
+  );
+}
