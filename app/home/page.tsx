44c1d542f5482b3324d69ed6cf302b1aaf76c001
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-10-10 08:36:38 +0000

    #3 [design] MarketInfoCard / react-icons 추가

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,7 +1,15 @@
-export default function Home() {
+import MaketInfoCard from "@/components/common/MaketInfoCard";
+const Home = () => {
+  const info = {
+    name: '롯데리아',
+    rate: 4.6,
+  }
   return (
     <div className="flex h-screen justify-center items-center">
       <div className="flex h-3/6 w-screen border-b-2 border-gray-500"></div>
+      <MaketInfoCard info={info} />
     </div>
   );
 }
+
+export default Home
