b97ac77efb5468f9f9a62f2ac22e150de91ea72f
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-10-16 09:43:27 +0000

    [fix] 오타 수정

--- components/home/Banner.tsx
+++ components/home/Banner.tsx
@@ -1,7 +1,6 @@
 const Banner = () => {
   return (
     <div className="w-full h-[100px] bg-slate-500 rounded-2xl flex justify-center items-center">
-      {/*임시 배너입니다*/}
       Slide Banner  
     </div>
   )
