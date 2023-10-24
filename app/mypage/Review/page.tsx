ab5b2b21c15a50a732cc325b5a292661fb02c318
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-10-24 11:06:08 +0900

    [feat] 리뷰 페이지 상단 탭메뉴/ 토글메뉴 애니메이션 완료

--- app/mypage/Review/page.tsx
+++ app/mypage/Review/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 'use client'
 import React, { useState, useEffect } from 'react';
 import TabMenu from '@/components/mypage/TabMenu';
@@ -35,70 +34,5 @@ const Review = () => {
     </div>
   )
 }
-=======
-'use client';
-import React, { useState, useEffect } from 'react';
-import '/lib/animations.css';
-
-const Review = () => {
-  const [selectedTab, setSelectedTab] = useState('deliveryAndTogo');
-
-  useEffect(() => {
-    console.log(selectedTab);
-  }, [selectedTab]);
-
-  const handleTabChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    setSelectedTab(e.target.value);
-  };
-
-  return (
-    <div className="bg-yogrey">
-      <div className="flex bg-white pl-4 pr-4">
-        <div className="w-[50%]">
-          <input
-            type="radio"
-            id="deliveryAndTogo"
-            value="deliveryAndTogo"
-            style={{ display: 'none' }}
-            checked={selectedTab === 'deliveryAndTogo'}
-            onChange={handleTabChange}
-          />
-          <label htmlFor="deliveryAndTogo">
-            <p
-              className={`text-center p-3 ${selectedTab === 'deliveryAndTogo' ? 'font-bold' : ''}`}
-            >
-              배달/포장
-            </p>
-          </label>
-        </div>
-        <div className="w-[50%]">
-          <input
-            type="radio"
-            id="yomart"
-            value="yomart"
-            style={{ display: 'none' }}
-            checked={selectedTab === 'yomart'}
-            onChange={handleTabChange}
-          />
-          <label htmlFor="yomart">
-            <p className={`text-center p-3 ${selectedTab === 'yomart' ? 'font-bold' : ''}`}>
-              요마트
-            </p>
-          </label>
-        </div>
-      </div>
-      <div className="pl-4 pr-4">
-        <div
-          className={`flex justify-end ${
-            selectedTab === 'deliveryAndTogo'
-              ? 'underline-animation-to-left'
-              : 'underline-animation-to-right'
-          } bg-black h-[3px] w-1/2`}
-        ></div>
-      </div>
-    </div>
-  );
-};
->>>>>>> 8706a59 ([feat] 리뷰 관리 페이지 메뉴 탭 애니메이션)
 
 export default Review;
