c80199b341bf98b430402a234097cda6952a1da9
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-10-28 22:54:34 +0900

    [rename] 폴더명을 소문자로 변경

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const login = () => {
+
+  return(
+    <div>
+      <img src="/public/img/yogiyo_new_logo.svg" alt="Your SVG" />
+    </div>
+  )
+}
+
+export default login;
