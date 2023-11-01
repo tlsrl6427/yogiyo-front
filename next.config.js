13525e202482213e1fa799ef5d677e847734c9c3
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-11-01 17:15:08 +0900

    [feat] app/pick/page.tsx export만 추가(임시)

--- next.config.js
+++ next.config.js
@@ -1,15 +1,5 @@
 //next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/:path*',
-        destination: `http://15.164.251.95:8080/:path*`,
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
