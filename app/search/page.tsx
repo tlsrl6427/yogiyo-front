c2a1d660208a4aef4c7fea3a9e221b523c4c1d59
THISISHYEONJIN <dontsendloveletters@gmail.com>
2023-11-03 08:59:14 +0000

    [feat] 검색 페이지 작업 중 - 로컬스토리지 최근 검색어 저장

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,12 @@
+import LatestKeyword from '@/components/search/LatestKeyword';
 import SearchHeader from '@/components/search/SearchHeader';
 
 const Search = () => {
   return (
     <div className="">
       <SearchHeader />
+      <div className='h-[50px]' />
+      <LatestKeyword />
     </div>
   );
 };
--- components/search/SearchHeader.tsx
+++ components/search/SearchHeader.tsx
@@ -2,6 +2,8 @@
 import { useRouter } from 'next/navigation';
 import { MdArrowBackIosNew } from 'react-icons/md';
 import { FiSearch } from 'react-icons/fi';
+import { useState } from 'react';
+import React from 'react';
 
 const SearchHeader = () => {
   const router = useRouter();
@@ -18,15 +20,37 @@ const SearchHeader = () => {
     left: '20px',
     fontSize: '1.1rem',
   };
+
+  const [nowKeyword, setKeyword] = useState('')
+
+  //검색할 때 마다 로컬스토리지에 검색어 저장
+  const formSearchHandler = (e: React.FormEvent) => {
+    e.preventDefault()
+
+    //로컬스토리이지에서 데이터 가져오기
+    const items = localStorage.getItem('recentSearchKeywords')
+    const recent = items ? JSON.parse(items) : []
+
+    //중복제거
+    const setObj = new Set([...recent, nowKeyword])
+    //배열로 다시 변환
+    const result = Array.from(setObj)
+    
+    const jsonData = JSON.stringify(result)
+    
+    localStorage.setItem('recentSearchKeywords', jsonData)
+  }
+
   return (
     <header className="fixed top-0 left-0 w-full h-[50px] flex justify-between items-center bg-white z-50">
       <MdArrowBackIosNew style={arrowStyle} onClick={() => router.back()} />
-      <form className="flex-1 px-[10px] relative">
+      <form onSubmit={formSearchHandler} className="flex-1 px-[10px] relative">
         <FiSearch style={searchIconStyle} />
         <input
           className="border rounded-[10px] border-slate-400 w-[100%] p-[8px] pl-[30px] text-[0.8rem]"
           type="text"
           placeholder="검색어를 입력해 주세요."
+          onChange={(e) => setKeyword(e.target.value)}
         />
       </form>
     </header>
