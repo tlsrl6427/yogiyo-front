aa7513b53d9734acbd1e259fcb57f078b91ddda6
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-11-09 23:59:01 +0900

    [feat] authcode 를 백엔드로 전달

--- services/api.ts
+++ services/api.ts
@@ -22,6 +22,7 @@ export const getNaverAuth = async (req: NaverAuth) => {
   window.location.href=`${baseURL}?response_type=code&client_id=${req.client_id}&redirect_uri=${req.redirect_uri}&state=${req.state}`
 };
 
+/*
 export const getGoogleAuthCode = () => {
   const client_id = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
   const redirect_uri = process.env.NEXT_PUBLIC_LOGIN_REDIRECT_URI;
@@ -31,8 +32,10 @@ export const getGoogleAuthCode = () => {
   const callLoginWindow = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`;
   window.location.href = callLoginWindow;
 };
+*/
 
-export const postGoogleAuthCode = async (authSource: any) => {
-  const res = await baseAxiosInstance.post('/socialLogin', authSource);
+export const getAccessToken = async (reqbody: SocialLogin) => {
+  console.log(reqbody);
+  const res = await baseAxiosInstance.post('/memberLogin', reqbody);
   console.log(res);
-};
+}
