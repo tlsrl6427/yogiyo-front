fadf3e53ac1b2f9f63aa63123f4fe3e566ee9233
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-11-09 23:59:01 +0900

    [feat] authcode 를 백엔드로 전달

--- app/loading/[provider]/page.tsx
+++ app/loading/[provider]/page.tsx
@@ -1,11 +1,25 @@
 'use client';
 import { useEffect, useState } from 'react';
 import { DynamicRoute } from '@/lib/types';
+import { useSearchParams } from 'next/navigation';
+import { getAccessToken } from '@/services/api';
 
 const Loading = ({params}: DynamicRoute) => {
   const [user, setUser] = useState({});
 
-  console.log(params.provider);
+  const queryString = useSearchParams();
+  const code = queryString.get('code');
+  const state = queryString.get('state');
+
+  useEffect(()=> {
+    const reqAuth = {
+      email: null,
+      password: null,
+      authCode: code,
+      provider: params.provider
+    }
+    getAccessToken(reqAuth);
+  },[])
 
   return (
     <div className="w-screen h-screen bg-yopink">
