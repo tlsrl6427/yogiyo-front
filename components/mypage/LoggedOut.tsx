b33749d30b524bac8e9c7bb19a14d7fcff704201
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-11-10 17:12:55 +0900

    [merge]

--- components/mypage/LoggedOut.tsx
+++ components/mypage/LoggedOut.tsx
@@ -1,22 +1,9 @@
 import { userAtom } from '@/recoil/state';
 import React from 'react';
 import { useRecoilState } from 'recoil';
-import { getGoogleAuthCode } from '@/services/loginAPI';
 
 const LoggedOut = () => {
   const [user, setUser] = useRecoilState(userAtom);
-
-  const handleLogin = () => {
-    getGoogleAuthCode();
-
-    setUser({
-      nickname: 'testuser',
-      email: 'test@gmail.com',
-      phone: 'unknown',
-      authAt: 'unknown',
-      providerType: 'unknown',
-    });
-  };
 
   return (
     <div className="">
@@ -25,7 +12,7 @@ const LoggedOut = () => {
         <p className="text-sm text-slate-600">로그인 후 매일 새로운 할인을 만나보세요.</p>
         <p className="text-sm text-slate-600">지금 가입하면 특별 추가 혜택까지!</p>
         <div className="flex justify-center mt-3 pb-5">
-          <button className="w-1/2 pt-4 pb-4 rounded-xl bg-yopink text-white" onClick={handleLogin}>
+          <button className="w-1/2 pt-4 pb-4 rounded-xl bg-yopink text-white" >
             로그인/회원가입
           </button>
         </div>
