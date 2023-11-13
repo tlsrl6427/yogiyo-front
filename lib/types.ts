d9eb0d55aa5b6f16e0fa39898e458987877a1760
정수아 <sua@jeongsuaui-MacBookAir.local>
2023-11-13 15:26:18 +0900

    [fix] 요청 헤더 추가

--- lib/types.ts
+++ lib/types.ts
@@ -45,6 +45,8 @@ export interface User {
 }
 
 export interface SocialLogin {
+  email : string | null,
+  password : string | null,
   authCode : string;
   providerType : string;
 }
--- services/loginAPI.ts
+++ services/loginAPI.ts
@@ -5,18 +5,6 @@ export const baseAxiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
-/*
-export const getNaverAuth = async () => {
-  axios
-    .get(
-      'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=0o_XScx3lU6SBOFvKmsc&redirect_uri=http://localhost:3000/&state=false',
-    )
-    .then(() => {
-      console.log('abc');
-    });
-};
-*/
-
 export const getNaverAuth = (req: ReqAuth) => {
   const baseURL = 'https://nid.naver.com/oauth2.0/authorize';
   window.location.href=`${baseURL}?response_type=${req.code}&client_id=${req.client_id}&redirect_uri=${req.redirect_uri}&state=${req.state}`
@@ -28,20 +16,12 @@ export const getKakaoAuth = async (req: ReqAuth) => {
   console.log(res)
 }
 
-/*
-export const getGoogleAuthCode = () => {
-  const client_id = process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID;
-  const redirect_uri = process.env.NEXT_PUBLIC_LOGIN_REDIRECT_URI;
-  const response_type = 'code';
-  const scope = 'email profile';
-
-  const callLoginWindow = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=${response_type}&scope=${scope}`;
-  window.location.href = callLoginWindow;
-};
-*/
-
 export const getAccessToken = async (reqbody: SocialLogin) => {
-  const res = await baseAxiosInstance.post('/memberLogin', reqbody);
-  //const res = await axios.post('https://yogiyo-clone.shop/memberLogin', reqbody);
+  const headers = {
+    'Content-Type': 'application/json',
+    'Content-Length': 162,
+    'Host': 'yogiyo-front.vercel.app'
+  };
+  const res = await baseAxiosInstance.post('/memberLogin', reqbody, {headers});
   console.log(res);
 }
