8e875ac427188c1efddb8916b5d35a07d6763924
THISISHYEONJIN <dontsendloveletters@gmail.com>
2024-01-03 08:49:20 +0000

    [feat] 가게 상세페이지 ui작업

--- components/detail/MiddleTitle.tsx
+++ components/detail/MiddleTitle.tsx
@@ -1,5 +1,5 @@
 'use client';
-import { FaStar } from 'react-icons/fa';
+import { FaStar, FaHeart } from 'react-icons/fa';
 import { SlArrowRight } from 'react-icons/sl';
 import { HiOutlineSpeakerphone } from 'react-icons/hi';
 import { useState } from 'react';
@@ -8,10 +8,25 @@ import Link from 'next/link';
 const MiddleTitle = () => {
   const [tap, setTap] = useState(0);
 
+  const listStyled = `
+    text-slate-500
+    font-bold
+  `
+
   return (
     <div className="">
-      <div className="px-4 pt-4">
+      <div className="px-4 pt-4 relative">
         <p className="text-[1.8rem] font-bold">가게이름</p>
+        <div 
+          className='absolute top-4 right-4'
+          onClick={() => {}}
+        >
+          <FaHeart style={{
+            fontSize: '1.8rem'
+          }}
+          color={'#f00'} />
+          <p className='text-[1.1rem] text-red-800 font-bold'>237</p>
+        </div>
       </div>
       <div className="flex items-center gap-2 px-4">
         <span className="text-[1.5rem] font-bold flex items-center gap-1">
@@ -23,6 +38,7 @@ const MiddleTitle = () => {
           <span className="">476</span>
           <SlArrowRight style={{ fontSize: '0.8rem' }} />
         </div>
+
       </div>
       <div className="pt-6">
         <div className="flex justify-around pt-4 border-b">
@@ -44,7 +60,32 @@ const MiddleTitle = () => {
           </span>
         </div>
       </div>
-      <div className="px-4 py-2">
+      <div className="p-4">
+        <div className='flex mb-4'>
+          <ul className='flex flex-col gap-2 w-full'>
+            <li className='flex gap-4'>
+              <p className={listStyled}>배달정보</p>
+              <div className='p-4 rounded-2xl bg-red-50 flex-1'>
+                <p className='text-red-500 text-[1.2rem] font-black'>40~45분 <span className='text-[1rem]'>가게배달</span></p>
+                <p>3,000원</p>
+                <p>요기패스는 배달비 무료</p>
+              </div>
+            </li>
+            <li className='flex gap-4'>
+              <p className={listStyled}>할인혜택</p>
+              <p className='text-red-500 font-bold'>15,000원 이상 주문 시 2,000원 할인</p>
+            </li>
+            <li className='flex gap-4'>
+              <p className={listStyled}>최소주문</p>
+              <p>14,000원</p>
+            </li>
+            <li className='flex gap-4'>
+              <p className={listStyled}>가게위치</p>
+              <p>1.2km</p>
+            </li>
+          </ul>
+        </div>
+
         {}
         <Link
           href={``}
