79e9266dc22360fca84b74f00c4075c09276f47d
정수아 <sua@jeongsuaui-MacBookAir.local>
2024-02-19 23:59:25 +0900

    [refactor] order 페이지 컴포넌트 분리

--- app/order/page.tsx
+++ app/order/page.tsx
@@ -83,66 +83,23 @@ const Order = () => {
 
   return (
     <div className="p-4">
+      
       <div className="flex flex-row text-sm pt-2 pb-2 border-b-2 border-grey1">
         <span className="pr-1 font-semibold">가게배달</span>
         <span>{`${mintime}~${maxtime}분 후 도착`}</span>
       </div>
-      <div className="pt-2 pb-2 min-h-[75px]">
-        <div className="flex">
-          <span className="mt-[3px]">
-            <HiOutlineHome />
-          </span>
-          <div className="flex flex-col pr-2 pl-2">
-            <p>집(으)로 배달</p>
-            <p className="text-sm">집주소</p>
-          </div>
-          <span className="mt-[3px]">
-            <HiOutlineChevronRight />
-          </span>
-        </div>
-      </div>
+
+      <Address />
       <Cart />
-      <div className="rounded-lg border p-4 mt-4">
-        <p className="font-bold">주문요청사항</p>
-        <div className="flex pt-4 pb-4 border-b">
-          <input type="checkbox" id="checkDoor" checked={door} onChange={handleCheckboxChange} />
-          <div className="pl-2">
-            <label htmlFor="checkDoor">문 앞에 놓고, 문자주세요.</label>
-          </div>
-        </div>
-        <div className="flex pt-4 pb-4 border-b">
-          <input type="checkbox" id="checkSpoon" checked={spoon} onChange={handleCheckboxChange} />
-          <div className="pl-2">
-            <label htmlFor="checkSpoon">일회용 수저, 포크가 필요해요.</label>
-            <p className="text-green-600 text-sm">
-              지구를 지키는 작은 실천! <span className="font-bold">현재까지 140회 참여</span>
-            </p>
-          </div>
-        </div>
-        <div className="flex pt-4">
-          <div className="flex-1 text-grey4">요청사항을 선택하세요</div>
-          <HiOutlineChevronRight style={{ margin: 'auto' }} />
-        </div>
-      </div>
-      <div className="mt-4">
-        <div className="flex pt-[6px]">
-          <div>상품금액</div>
-          <div className="ml-auto">원</div>
-        </div>
-        <div className="flex pt-2 pb-[6px]">
-          <div>배달금액</div>
-          <div className="ml-auto">원</div>
-        </div>
-        <div className="flex pt-4 pb-4 font-bold border-t border-t-grey2">
-          <div>총 결제금액</div>
-          <div className="ml-auto">원</div>
-        </div>
-      </div>
+      <OrderNotes />
+      <Prices />
+      
       <div className="p-6 text-sm bg-grey8 text-grey5 mt-2">
         <p>개인정보 제3자 제공 내용 및 결제에 동의합니다.</p>
         <p>최소주문금액은 배달요금/일회용컵 보증금을 제외한 금액입니다.</p>
         <p>배달요금에는 할인수단이 적용되지 않습니다? (단, 배달요금 관련 할인/선물은 적용)</p>
       </div>
+
     </div>
   );
 };
@@ -195,5 +152,70 @@ const Cart = () => {
           <div>메뉴추가하기</div>
         </div>
       </div>
+  )
+}
+
+const OrderNotes = () => {
+  return(
+    <div className="rounded-lg border p-4 mt-4">
+        <p className="font-bold">주문요청사항</p>
+        <div className="flex pt-4 pb-4 border-b">
+          <input type="checkbox" id="checkDoor" checked={door} onChange={handleCheckboxChange} />
+          <div className="pl-2">
+            <label htmlFor="checkDoor">문 앞에 놓고, 문자주세요.</label>
+          </div>
+        </div>
+        <div className="flex pt-4 pb-4 border-b">
+          <input type="checkbox" id="checkSpoon" checked={spoon} onChange={handleCheckboxChange} />
+          <div className="pl-2">
+            <label htmlFor="checkSpoon">일회용 수저, 포크가 필요해요.</label>
+            <p className="text-green-600 text-sm">
+              지구를 지키는 작은 실천! <span className="font-bold">현재까지 140회 참여</span>
+            </p>
+          </div>
+        </div>
+        <div className="flex pt-4">
+          <div className="flex-1 text-grey4">요청사항을 선택하세요</div>
+          <HiOutlineChevronRight style={{ margin: 'auto' }} />
+        </div>
+      </div>
+  )
+}
+
+const Address = () => {
+  return(
+    <div className="pt-2 pb-2 min-h-[75px]">
+      <div className="flex">
+        <span className="mt-[3px]">
+          <HiOutlineHome />
+        </span>
+        <div className="flex flex-col pr-2 pl-2">
+          <p>집(으)로 배달</p>
+          <p className="text-sm">집주소</p>
+        </div>
+        <span className="mt-[3px]">
+          <HiOutlineChevronRight />
+        </span>
+      </div>
+    </div>
+  )
+}
+
+const Prices = () => {
+  return(
+    <div className="mt-4">
+      <div className="flex pt-[6px]">
+        <div>상품금액</div>
+        <div className="ml-auto">원</div>
+      </div>
+      <div className="flex pt-2 pb-[6px]">
+        <div>배달금액</div>
+        <div className="ml-auto">원</div>
+      </div>
+      <div className="flex pt-4 pb-4 font-bold border-t border-t-grey2">
+        <div>총 결제금액</div>
+        <div className="ml-auto">원</div>
+      </div>
+    </div>
   )
 }
