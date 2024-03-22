4f6b756e1c8567a1b8e591b9e545c9f35003ee8a
정수아 <sua@jeongsuaui-MacBookAir.local>
2024-03-22 16:46:40 +0900

    [remove] 기존 order detail 페이지(쿼리스트링 이용)삭제

--- app/order/detail/[orderId].tsx
+++ app/order/detail/[orderId].tsx
@@ -1,25 +1,34 @@
-import { DynamicRoute } from "@/types/types"
-import { useEffect } from "react";
+import { DynamicRoute, Order } from "@/types/types"
+import { useEffect, useState } from "react";
 
 import DeliState from "@/components/order/detail/deliState";
 import ShopInfo from "@/components/order/detail/shopInfo";
 import Receipt from "@/components/order/detail/receipt";
+import { getOrderDetail } from "@/services/orderAPI";
 
 const OrderDetailDynamic = ({param}: DynamicRoute) => {
   const orderId = param;
+  const [orderInfo, setOrderInfo] = useState<Order | null>(null);
   
   useEffect(()=>{
-
+    handleAPI()
   },[])
 
+  const handleAPI = async () => {
+    const orderDetail = await getOrderDetail(orderId);
+    setOrderInfo(orderDetail.data)
+  }
+
   return(
-    <>
-    <div className='w-screen min-h-screen bg-grey7'>
-      <DeliState />
-      <ShopInfo shopId={shopId} shopName={shopName}/>
-      <Receipt />
+    <div>
+      {orderInfo?
+      <div className='w-screen min-h-screen bg-grey7'>
+        <DeliState />
+        <ShopInfo orderInfo={orderInfo}/>
+        <Receipt />
+      </div>
+      :<></>}
     </div>
-    </>
   )
 }
 
--- components/order/detail/shopInfo.tsx
+++ components/order/detail/shopInfo.tsx
@@ -1,20 +1,20 @@
 
+import { Order } from "@/types/types";
 import { useRouter } from "next/navigation";
 
 interface Props {
-  shopId : string,
-  shopName : string
+  orderInfo: Order
 }
 
-const ShopInfo = ({shopId, shopName}: Props) => {
+const ShopInfo = ({orderInfo}: Props) => {
   const router = useRouter();
   const handleMoveShop = () => {
-    console.log(`shopid: ${shopId}`)
-    router.push(`/detail?id=${shopId}`)
+    console.log(`shopid: ${orderInfo.shopId}`)
+    router.push(`/detail?id=${orderInfo.shopId}`)
   }
   return(
     <div className='px-4 py-6 bg-white min-h-10 border-b border-grey2'>
-    <div className='pb-3 font-semibold text-[1.3rem]' onClick={handleMoveShop}>{shopName}</div>
+    <div className='pb-3 font-semibold text-[1.3rem]' onClick={handleMoveShop}>{orderInfo.shopName}</div>
     <div className='flex'>
       <div className='flex-1 p-2 mr-1 leading-7 text-center rounded-xl border border-pink1 text-pink1'>재주문</div>
       <div className='flex-1 p-2 ml-1 leading-7 text-center rounded-xl border border-black'>리뷰쓰기</div>
