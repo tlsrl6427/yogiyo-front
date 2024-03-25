b3edf929a95ed3692eb6d0502f80e563a67fa8b6
THISISHYEONJIN <50274369+hyeonjin-dev@users.noreply.github.com>
2024-03-25 07:42:32 +0000

    [fix] 코드 일관성 및 루트페이지 렌더링 방식 변경 - app/page.tsx및 app/home/page.tsx의 함수 컴포넌트 이름 변경 - app/page.tsx의 주소 세팅 로직을 모두 별개의 app/ClientComponent.tsx로 이동 - app/layout.tsx에서 ClientComponent호출

--- app/page.tsx
+++ app/page.tsx
@@ -1,158 +1,23 @@
-'use client';
-import Head from 'next/head';
 import './globals.css';
 import Header from '@/components/header/Header';
 import Footer from '@/components/common/Footer';
-import Homes from './home/page';
-import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
-import { currentAddress, currentCoord, currentRegionCode, thisAddressId, userAddress } from '@/recoil/address';
-import { loadingState, userInfoAtom } from '@/recoil/state';
-import { useState, useEffect } from 'react';
-import { fetchAddress } from '@/lib/fetchAddress';
-import SplashPage from '@/components/common/SplashPage';
-import { useRouter } from 'next/navigation';
-import LoginPage from '@/components/common/LoginPage';
-import ServerLoginCheck from '@/components/common/ServerLoginCheck';
+import HomePage from './home/page';
+import Head from 'next/head';
 
-
-const Home = () => {
-
-  const [curCoord, setCurCoord] = useRecoilState(currentCoord);
-  const [regionCode, setRegionCode] = useRecoilState(currentRegionCode);
-  const [curAdd, setCurAdd] = useRecoilState(currentAddress);
-  const setThisAdd = useSetRecoilState(thisAddressId);
-  const setMemberAddress = useSetRecoilState(userAddress);
-  const userInfo = useRecoilValue(userInfoAtom);
-  
-  //로딩 완료 체크
-  const [locationLoaded, setLocationLoaded] = useState(false);
-  const [regionCodeLoaded, setRegionCodeLoaded] = useState(false);
-  const [isLoading, setIsLoading] = useRecoilState(loadingState);
-  const router = useRouter();
-
-  useEffect(() => {
-    // 현재 유저의 위치 찾기
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const lat = position.coords.latitude;
-          const lng = position.coords.longitude;
-          setCurCoord({ lat, lng });
-          setLocationLoaded(true);
-        },
-        (error) => {
-          // 오류날 경우 or 유저가 위치추적권한을 허용하지 않을 경우 임의 좌표 설정
-          setCurCoord({ lat: 37.566826, lng: 126.9786567 }); // Default to Seoul
-          setLocationLoaded(true);
-        },
-      );
-    }else{
-      setLocationLoaded(true);
-    }
-
-  }, []);
-
-  const [cookies, setCookies] = useState(null);
-  useEffect(() => {
-    const fetchCookies = async () => {
-      const res = await fetch('/api/getCookies');
-      const data = await res.json();
-      setCookies(data);
-    };
-    fetchCookies();
-    console.log(cookies)
-  }, []);
-
-  useEffect(() => {
-    if (!curCoord) return; // 현재 유저의 위치 찾기 전이라면 return
-
-    const kakaoMapScript = document.createElement('script');
-    kakaoMapScript.async = false;
-    kakaoMapScript.src = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_APP_KEY}&autoload=false&libraries=services`;
-    document.head.appendChild(kakaoMapScript);
-
-    const onLoadKakaoAPI = () => {
-      const { kakao } = window;
-      kakao.maps?.load(() => {
-        // 주소-좌표 변환 객체를 생성
-        const geocoder = new kakao.maps.services.Geocoder();
-
-        // 좌표 설정
-        const latlng = new kakao.maps.LatLng(curCoord.lat, curCoord.lng);
-
-        // 좌표를 주소로 변환
-        geocoder.coord2Address(latlng.getLng(), latlng.getLat(), (result: any, status: any) => {
-          if (status === kakao.maps.services.Status.OK) {
-            const detailAddress = result[0].address.address_name;
-            setCurAdd(detailAddress);
-          }
-        });
-        const callback = (result: any, status: any) => {
-          if (status === kakao.maps.services.Status.OK) {
-            setRegionCode(result[0].code);
-            setRegionCodeLoaded(true);
-          }
-        };
-        geocoder.coord2RegionCode(curCoord.lng, curCoord.lat, callback);
-      });
-    };
-    kakaoMapScript.addEventListener('load', onLoadKakaoAPI);
-    //주소값 세팅되면 로딩완료
-
-    // '요기'세팅된 주소가 없을 경우 현재 주소값 적용
-    const optional = {
-      id: 0,
-      address: {
-        zipcode: '',
-        street: curAdd,
-        detail: '',
-      },
-      here: true,
-      code: regionCode,
-      addressType: '',
-      nickname: curAdd,
-      longitude: curCoord?.lng,
-      latitude: curCoord?.lat,
-    }
-    
-    fetchAddress(setMemberAddress, setThisAdd, optional);
-
-    // Cleanup
-    return () => {
-      kakaoMapScript.removeEventListener('load', onLoadKakaoAPI);
-    };
-  }, [curCoord]);
-
-  // 로딩 완료 확인
-  useEffect(() => {
-    if(locationLoaded && regionCodeLoaded){
-      setIsLoading(false);
-    }
-  }, [locationLoaded, regionCodeLoaded])
-
+const RootPage  = () => {
   return (
     <div>
       <Head>
         <title>yogiyo</title>
         <meta name="description" content="clone yogiyo"></meta>
       </Head>
-      {/* 로딩 유무 확인 - 로딩이 완료될 때 까지 스플래쉬 페이지 오픈 */}
-      {isLoading ? 
-        <SplashPage /> :
-
-        // 로그인 유무 확인 - 비로그인 시 로그인 페이지 오픈
-        // userInfo.isLogin ? 
-        <main className="flex flex-col w-full pt-[50px] pb-[70px]">
-          <Header />
-          <Homes />
-          <Footer />
-        </main> 
-        // :
-        // <LoginPage />
-      }
-
+      <main className="flex flex-col w-full pt-[50px] pb-[70px]">
+        <Header />
+        <HomePage />
+        <Footer />
+      </main>
     </div>
   );
 }
 
-export default Home;
+export default RootPage ;
