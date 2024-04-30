2b35aee8b65fb7cd0c8cb773cab247f1cc0c5c53
정수아 <sua@jeongsuaui-MacBookAir.local>
2024-04-30 17:08:28 +0900

    [fix] 각 경로(auth, footer, none)의 layout 파일을 역할에 맞게 수정함

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,9 +1,27 @@
 'use client'
+import { useEffect } from 'react';
+import { useRecoilValue } from 'recoil';
+import { userInfoAtom } from '@/recoil/state';
+import { useRouter } from 'next/navigation';
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
+  const user = useRecoilValue(userInfoAtom);
+  const router = useRouter();
+
+  useEffect(()=>{
+    if(!user.isLogin){
+      router.push('/login')
+    }
+  },[])
+
   return (
     <div>
-      <div>{children}</div>
+      {user.isLogin ? 
+        <>
+          <div>{children}</div>
+        </> :
+        <></>
+      }
     </div>
   );
 }
